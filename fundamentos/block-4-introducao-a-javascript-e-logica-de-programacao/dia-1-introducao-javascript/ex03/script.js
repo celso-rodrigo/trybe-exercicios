831da3b87ddc3bf5ed18c0912004b0c9196c346b
celsorodrigo <celsorodrigoma@gmail.com>
2022-04-06 15:11:34 -0300

    Adicionado ex03.

---
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/ex03/script.js
@@ -0,0 +1,11 @@
+// Crie uma costante chamada base e uma variável chamada height e atribua os seus respectivos valores: 5 e 8.
+// Crie uma constante chamada area e atribua a ela o resultado da multiplicação da base pela altura. Dica: lembre-se de usar sempre o console.log() para imprimir as variáveis e checar os resultados das operações!
+// Crie uma constante chamada perimeter e atribua a ela a soma de todos os lados do retângulo.
+
+const base = 5;
+let height = 8;
+const area = base*height;
+const perimeter = 2*height+base
+
+console.log(`O Triângulo tem uma base de ${base} e uma altura de ${height}.`)
+console.log(`A área do triângulo é de ${area} e o seu perímetro é de ${perimeter}.`)
