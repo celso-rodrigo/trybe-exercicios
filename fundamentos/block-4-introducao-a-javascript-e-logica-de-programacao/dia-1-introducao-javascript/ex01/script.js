1e0a19f7b07428e355854101c2e09bbf49980fb4
celsorodrigo <celsorodrigoma@gmail.com>
2022-04-06 14:54:20 -0300

    Adicionada pasta do bloco 4.1, ex01 já concluído e pasta do ex02.

---
+++ fundamentos/block-4-introducao-a-javascript-e-logica-de-programacao/dia-1-introducao-javascript/ex01/script.js
@@ -0,0 +1,14 @@
+const myName = "Celso Rodrigo";
+const birthCity = "Campina Grande";
+let birthYear = 1998;
+
+console.log(`Olá! Me chamo ${myName} e nasci em ${birthCity} no ano de ${birthYear}.`)
+
+birthYear = 2030;
+
+console.log(birthYear);
+
+birthCity = "João Pessoal";
+
+// Erro devido a tentativa de alterar um valor do tipo constante
+console.log(birthCity);
