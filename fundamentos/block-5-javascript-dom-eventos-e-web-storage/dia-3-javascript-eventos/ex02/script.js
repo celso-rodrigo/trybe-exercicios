02292dfa473ff3171386dffd1b2b0c23b85922ef
celsorodrigo <celsorodrigoma@gmail.com>
2022-04-18 18:05:28 -0300

    Adicionada terceira e quarta parte da atividade dos exercícios do dia.

--- fundamentos/block-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ex02/script.js
+++ fundamentos/block-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ex02/script.js
@@ -22,13 +22,14 @@ createDaysOfTheWeek();
 
 const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
 
+const holidays = [24, 25, 31];
+const fridays = [4, 11, 18, 25];
+
 createDays();
 
 function createDays(){
   const month = document.getElementById('days');
-  const holidays = [24, 25, 31];
-  const fridays = [4, 11, 18, 25];
-  
+
   for (let days of dezDaysList){
     const dayMaker = document.createElement('li');
     dayMaker.innerText = days;
@@ -60,6 +61,83 @@ function feriadoBtn(phrase) {
   const divFinder = document.querySelector('.buttons-container');
   btnMaker.innerText = phrase;
   btnMaker.id = 'btn-holiday';
-  console.log(divFinder)
   divFinder.appendChild(btnMaker);
 }
+
+// Implemente uma função que adicione ao botão "Feriados" um evento de "click" que muda a cor de fundo dos dias que possuem a classe "holiday" .
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial com a cor "rgb(238,238,238)" .
+
+const btnHoliday = document.getElementById('btn-holiday');
+btnHoliday.addEventListener("click", feriadoBtnColor);
+
+function feriadoBtnColor() {
+  const btnHolidayColor = document.getElementsByClassName('holiday');
+
+  for (let btnHd of btnHolidayColor){
+    if (btnHd.style.backgroundColor != 'rgba(164, 230, 164, 0.44)'){
+      btnHd.style.backgroundColor = 'rgba(164, 230, 164, 0.44)';
+    } else {
+      btnHd.style.backgroundColor = 'rgb(238,238,238';
+    }
+  }
+} 
+
+// Implemente uma função que receba como parâmetro a string "Sexta-feira" e crie dinamicamente um botão com o nome "Sexta-feira".
+// Adicione a este botão o ID "btn-friday" .
+// Adicione este botão como filho/filha da tag <div> com classe "buttons-container" .
+
+fridaysBtn('Sexta-feira')
+function fridaysBtn(phrase) {
+  const btnMaker = document.createElement('button');
+  const divFinder = document.querySelector('.buttons-container');
+  btnMaker.innerText = phrase;
+  btnMaker.id = 'btn-friday';
+  divFinder.appendChild(btnMaker);
+}
+
+// Implemente uma função que adicione ao botão "Sexta-feira" um evento de "click" que modifica o texto exibido nos dias que são Sexta-feira.
+// É interessante que este botão possua também a lógica inversa. Ao ser clicado novamente ele retorna à configuração inicial exibindo os dias.
+
+const btnFriday = document.getElementById('btn-friday');
+btnFriday.addEventListener("click", sextaBtnText);
+
+function sextaBtnText() {
+  const btnFridayText = document.getElementsByClassName('friday');
+  for (let cont = 0; cont < btnFridayText.length; cont += 1){
+    if (btnFridayText[cont].innerText != 'Sextou!'){
+      btnFridayText[cont].innerText = 'Sextou!';
+    } else {
+      btnFridayText[cont].innerText = fridays[cont]
+    }
+  }
+} 
+
+// Implemente duas funções que criem um efeito de "zoom". Ao passar o ponteiro do mouse em um dia do mês no calendário, o texto desse dia deve aumentar e, quando o ponteiro do mouse sair do dia, o texto deve retornar ao tamanho original.
+
+
+
+// Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+
+
+// Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
+// O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
+// O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
+
+
+
+// Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected , ela estará selecionada.
+// Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
+
+
+
+// Implemente uma função que adiciona um evento que, ao clicar em um dia do mês no calendário, atribua a este dia a cor da legenda da sua tarefa selecionada.
+// Ao clicar novamente no dia com a cor da legenda, a sua cor deverá voltar à configuração inicial rgb(119,119,119) .
+
+
+
+// Vamos adicionar compromissos ao seu calendário? Implemente uma função que, ao digitar um compromisso na caixa de texto "COMPROMISSOS", adiciona o item à lista "MEUS COMPROMISSOS" ao clicar no botão "ADICIONAR".
+// Se nenhum caractere for inserido no campo input , a função deve retornar um alert com uma mensagem de erro ao clicar em "ADICIONAR".
+// Ao pressionar a tecla "enter" o evento também deverá ser disparado.
+// Dica - Propriedade: key .
