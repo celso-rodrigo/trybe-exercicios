3310a927d0542c2ace3a4ef7d00e5371532d7445
celsorodrigo <celsorodrigoma@gmail.com>
2022-04-18 16:16:17 -0300

    Adicionada primeira parte da atividade dos exercícios do dia.

---
+++ fundamentos/block-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ex02/script.js
@@ -0,0 +1,51 @@
+function createDaysOfTheWeek() {
+  const weekDays = ['Domingo', 'Segunda', 'Terça', 'Quarta', 'Quinta', 'Sexta', 'Sábado']
+  const weekDaysList = document.querySelector('.week-days');
+
+  for (let index = 0; index < weekDays.length; index += 1) {
+    const days = weekDays[index];
+    const dayListItem = document.createElement('li');
+    dayListItem.innerHTML = days;
+
+    weekDaysList.appendChild(dayListItem);
+  };
+};
+
+createDaysOfTheWeek();
+
+// Escreva seu código abaixo.
+
+// O array dezDaysList contém os dois últimos dias de novembro e os dias do mês de dezembro. Desenvolva uma função que crie dinamicamente cada dia do calendário e os adicione como filhos/filhas da tag <ul> com ID "days" . Note que os dias 29 e 30 de novembro estão no array pois representam respectivamente Domingo e Segunda-feira.
+// Os dias devem estar contidos em uma tag <li> , e todos devem ter a classe day . Ex: <li class="day">3</li>
+// Os dias 24, 25 e 31 são feriados e, além da classe day , devem conter também a classe holiday . Ex: <li class="day holiday">24</li>
+// Os dias 4, 11, 18 e 25 são Sexta-feira. Eles devem conter a classe day e a classe friday . Ex: <li class="day friday">4</li>
+
+const dezDaysList = [29, 30, 1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25, 26, 27, 28, 29, 30, 31];
+
+createDays();
+
+function createDays(){
+  const month = document.getElementById('days');
+  const holidays = [24, 25, 31];
+  const fridays = [4, 11, 18, 25];
+  
+  for (let days of dezDaysList){
+    const dayMaker = document.createElement('li');
+    dayMaker.innerText = days;
+    dayMaker.classList = 'days';
+
+    for (let holidayDays of holidays){
+      if (days === holidayDays){
+        dayMaker.classList += ' holiday';
+      }
+    }
+
+    for (let fridayDays of fridays){
+      if (days === fridayDays){
+        dayMaker.classList += ' friday';
+      }
+    }
+
+    month.appendChild(dayMaker);
+  }
+}
