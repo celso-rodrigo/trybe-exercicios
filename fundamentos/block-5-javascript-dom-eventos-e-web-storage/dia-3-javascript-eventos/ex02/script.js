9f2adbb8472866d45f401e1292b5dd13bb72e188
celsorodrigo <celsorodrigoma@gmail.com>
2022-04-18 19:32:23 -0300

    Adicionada sétima parte da atividade dos exercícios do dia.

--- fundamentos/block-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ex02/script.js
+++ fundamentos/block-5-javascript-dom-eventos-e-web-storage/dia-3-javascript-eventos/ex02/script.js
@@ -132,22 +132,36 @@ function unZoomDay(originElement) {
 // Implemente uma função que adiciona uma tarefa personalizada ao calendário. A função deve receber como parâmetro a string com o nome da tarefa (ex: "cozinhar") e criar dinamicamente um elemento com a tag <span> contendo a tarefa.
 // O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
 
+const taksFinder = document.querySelector('.my-tasks')
 const addTaskBtn = document.getElementById('btn-add');
 const addTaskText = document.getElementById('task-input');
 addTaskBtn.addEventListener('click', taskManager)
 
 function taskManager(task) {
-  const taksFinder = document.querySelector('.my-tasks')
-  taskMaker = document.createElement('span');
-  taskMaker.innerText = addTaskText.value;
-  taksFinder.appendChild(taskMaker)
+  taskSpanMaker = document.createElement('span');
+  taskSpanMaker.innerText = addTaskText.value;
+  taksFinder.appendChild(taskSpanMaker)
 }
 
 // Implemente uma função que adiciona uma legenda com cor para a tarefa criada no exercício anterior. Esta função deverá receber como parâmetro uma string ("cor") e criar dinamicamente um elemento de tag <div> com a classe task .
 // O parâmetro cor deverá ser utilizado como cor de fundo da <div> criada.
 // O elemento criado deverá ser adicionado como filho/filha da tag <div> que possui a classe "my-tasks" .
 
+addTaskBtn.addEventListener('click', taskColor)
 
+function taskColor(cor) {
+  console.log('chamada')
+  const colorRandomizer = ['red', 'green', 'yellow', 'pink', 'orange', 'purple'];
+  let color = colorRandomizer[Math.floor(Math.random()*colorRandomizer.length)];
+  taskColorPlace(color)
+}
+
+function taskColorPlace(chamada) {
+  console.log('chamada')
+  taskDivMaker = document.createElement('div');
+  taskDivMaker.style.backgroundColor = chamada;
+  taksFinder.appendChild(taskDivMaker)
+}
 
 // Implemente uma função que adiciona um evento que, ao clicar no elemento com a tag <div> referente a cor da sua tarefa, atribua a este elemento a classe task selected , ou seja, quando sua tarefa possuir a classe task selected , ela estará selecionada.
 // Ao clicar novamente no elemento, a sua classe deverá voltar a ser somente task , ou seja, esta tarefa está deixando de ser uma tarefa selecionada.
