25b10404a413c27db2a7cbd65051b78f921dc623
celsorodrigo <celsorodrigoma@gmail.com>
2022-05-13 15:37:43 -0300

    Finalizado ex03.

---
+++ fundamentos/block-8-higher-order-functions-do-javascript-es6/ex03/script.js
@@ -0,0 +1,18 @@
+// Desenvolva uma HOF que retorna o resultado de um sorteio. Esta HOF irá gerar um número aleatório entre 1 e 5 recebendo como parâmetros o número apostado e uma função que checa se o número apostado é igual ao número sorteado. O retorno da sua HOF deve ser uma string (Ex: "Tente novamente" ou "Parabéns você ganhou").
+
+const oneToFive = (result, num) => {
+  let resultTxt;
+  if (result === num) {
+    resultTxt = 'Parabéns você ganhou';
+  } else {
+    resultTxt = 'Tente novamente';
+  }
+  return resultTxt;
+};
+
+const gamble = (func, num) => {
+  const result = Math.floor(Math.random() * 5 + 1);
+  return func(result, num);
+};
+
+console.log(gamble(oneToFive, 5));
