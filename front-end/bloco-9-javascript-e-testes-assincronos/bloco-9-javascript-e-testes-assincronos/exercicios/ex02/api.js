66c69775ddcfceb004c3e91ab3bbe54040f62ccc
celsorodrigo <celsorodrigoma@gmail.com>
2022-06-01 18:40:29 -0300

    Concluído exercício bônus.

--- front-end/bloco-9-javascript-e-testes-assincronos/bloco-9-javascript-e-testes-assincronos/exercicios/ex02/api.js
+++ front-end/bloco-9-javascript-e-testes-assincronos/bloco-9-javascript-e-testes-assincronos/exercicios/ex02/api.js
@@ -1,17 +1,28 @@
 const criptsUl = document.getElementById('cripts');
-const cryptoAPI = 'https://api.coincap.io/v2/assets';
+const cryptoEndP = 'https://api.coincap.io/v2/assets';
+const usdEndP = 'https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/latest/currencies/usd.min.json';
 const rakingSize = 10;
+let brlValue = 0;
+
+function getBrlPrice() {
+  const promesse = fetch(usdEndP)
+  promesse
+    .then((response) => response.json())
+    .then((data) => data.usd['brl'])
+    .then((brl) => brlValue = brl)
+    .catch((err) => alert(`Erro ao converter Dolar para BRL. \n ${err}`))
+}
 
 function setRanking(element) {
   const name = element.id.charAt(0).toUpperCase() + element.id.slice(1);
-  const price = `${element.priceUsd.split('.')[0]}.${element.priceUsd.split('.')[1].substring(0,2)}`;
+  const brlPrice = (brlValue * Number(element.priceUsd)).toFixed(2)
   const liMaker = document.createElement('li'); 
-  liMaker.innerText = `${name} (${element.symbol}): $${price}`;
+  liMaker.innerText = `${name} (${element.symbol}): R$${brlPrice}`;
   criptsUl.appendChild(liMaker);
 }
 
-async function getCriptRank() {
-  const info = await fetch(cryptoAPI)
+function getCriptRank() {
+  const info = fetch(cryptoEndP)
     .then((response) => response.json())
     .then((criptData) => criptData.data.filter((element, index) => {
       if (index < rakingSize) {
@@ -21,4 +32,4 @@ async function getCriptRank() {
     .catch((err) => alert(`Erro ao carregar aos moedas. \n ${err}`))
 }
 
-window.onload = () => getCriptRank();
+window.onload = () => getCriptRank(), getBrlPrice();
